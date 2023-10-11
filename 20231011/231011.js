32ce14939a911c81136321206e3e4d85dae7c748
m0a21071 <mail@m0a21071f7@edu.teu.ac.jp>
2023-10-11 17:52:23 +0900

    変数使ってみた

---
+++ 20231011/231011.js
@@ -0,0 +1,12 @@
+let variableA;
+variableA = "最初の代入A";
+console.log(variableA);
+
+let variableB = "最初の代入B";
+console.log(variableB);
+
+variableA = "二回目の代入A";
+console.log(variableA);
+
+variableA = variableB;
+console.log(variableA);
